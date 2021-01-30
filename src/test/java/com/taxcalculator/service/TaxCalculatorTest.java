31a37a807a671f35dc272398a16af07181b1e2ed
Brigi <pap.mbrigitta@gmail.com>
2021-01-30 15:18:43 +0200

    added unit tests

--- src/test/java/com/taxcalculator/service/TaxCalculatorTest.java
+++ src/test/java/com/taxcalculator/service/TaxCalculatorTest.java
@@ -1,9 +1,9 @@
 package com.taxcalculator.service;
 
-import com.taxcalculator.model.Employee;
-import com.taxcalculator.model.Intern;
-import com.taxcalculator.model.Salary;
+import com.taxcalculator.model.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,4 +15,30 @@ public class TaxCalculatorTest {
 
         assertEquals(salary.getTax(testEmployee), 320);
     }
+
+    @Test
+    void testTaxForPartTimeEmployee() {
+        Employee partTimeEmployee = new PartTimeEmployee("Adriana", 4000);
+        Salary salary = partTimeEmployee.getSalary();
+
+        assertEquals(salary.getTax(partTimeEmployee), 1040);
+    }
+
+    @Test
+    void testTaxForFullTimeEmployee() {
+        Employee fullTimeEmployee = new FullTimeEmployee("Mariana", 6300);
+        Salary salary = fullTimeEmployee.getSalary();
+
+        assertEquals(salary.getTax(fullTimeEmployee), 1838);
+    }
+
+    @Test
+    void testRuntimeException() {
+        String messageExcepted = "Invalid employee type";
+        Employee employee = null;
+
+        TaxCalculatorFactory taxCalculatorFactory = new TaxCalculatorFactory();
+        Assertions.assertThrows(RuntimeException.class, () -> taxCalculatorFactory.getTaxCalculator(employee), messageExcepted);
+    }
+
 }
